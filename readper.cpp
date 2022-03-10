1507352de71eb9d2f69c752f4062b4a7c97b1ce4
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2022-03-10 14:40:33 +0900

    1

---
+++ readper.cpp
@@ -0,0 +1,21 @@
+//extra Person class 읽고 출력하기
+#include <fstream>
+#include <string.h>
+#include <iostream>
+#include "person.h"
+using namespace std;
+
+const int MaxBufferSize = 200;
+
+void ReadPerson(char* filename)
+{
+
+}
+
+int main() {
+	char filename[20];
+	Person p;
+	cout << "Enter the file name:" << flush;
+	cin.getline(filename, 19);
+
+}
